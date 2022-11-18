8edc0b81bb1679324b89978610aab27f6e694b29
Joren Synaeve <joren.synaeve@thomasmore.be>
2022-11-18 15:56:55 +0100

    Bugfixes

--- app/file_helper.py
+++ app/file_helper.py
@@ -4,18 +4,20 @@ import os
 
 def delete_all_content(dir):
     """ Deletes all the content of a directory """
-    # Get the contents of the directory
-    contents = os.listdir(dir)
-    # For each file in the directory, delete it
-    for content in contents:
-            # Check if the file is a directory
-            if os.path.isdir(dir + "/" + content):
-                try:
-                    print("Deleting " + content)
-                # If so, delete the directory
-                    shutil.rmtree(dir)
-                except:
-                    print("Error while deleting " + content)
+    # Check if the directory exists
+    if os.path.exists(dir):
+        # Get the contents of the directory
+        contents = os.listdir(dir)
+        # For each file in the directory, delete it
+        for content in contents:
+                # Check if the file is a directory
+                if os.path.isdir(dir + "/" + content):
+                    try:
+                        print("Deleting " + content)
+                    # If so, delete the directory
+                        shutil.rmtree(dir)
+                    except:
+                        print("Error while deleting " + content)
 
 def make_dir(dir):
     """ Creates a directory if it does not exist """
--- app/index.py
+++ app/index.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import json_helper
 import github_helper
+import file_helper
 import codegrade_helper
 import course_constants
 import shutil
@@ -30,7 +31,7 @@ async def oop_root(payload: GithubPushPayload):
             # Get the assignment name from the config file in the dir
             assignment_name = github_helper.get_assignment_name_from_config(dir)
             # Delete all contents in the temp folder
-            shutil.rmtree(course_constants.TEMP_DIR)
+            file_helper.delete_all_content(course_constants.TEMP_DIR)
             # Download all from github to the temp folder
             github_helper.download_dir(dir, course_constants.TEMP_DIR)
             for course_id in course_constants.CODEGRADE_COURSE_IDS:
@@ -41,8 +42,7 @@ async def oop_root(payload: GithubPushPayload):
                     output.append('Updated assignment ' + assignment_name + ' on CodeGrade in course ' + str(course_id))
                 else:
                     output.append("Assignment " + assignment_name + " does not exist on Codegrade. Did not configure.")
+            # Delete all contents in the temp folder
+            file_helper.delete_all_content(course_constants.TEMP_DIR)
              
     return output
-
-if __name__ == '__main__':
-  app.run(debug=True)
