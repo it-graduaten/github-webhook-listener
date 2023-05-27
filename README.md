68c739606c2db3d7de389adbf7e73368097b3b26
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-05-27 20:42:45 +0200

    Change to Flask with vercel setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# github-webhook-listener
-This is a FastAPI python API which listens to the Webhooks of other repo's to update assignments on Codegrade.
--- app/course_constants.py
+++
@@ -1,2 +0,0 @@
-CODEGRADE_COURSE_IDS ="4041,4059"  # Course IDS for day and evening on Codegrade
-TEMP_DIR = "temp"  # Temp folder for downloading files from Github
