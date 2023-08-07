3cedddf1c583e72ff119cc9d89201493738d1c7a
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-08-07 11:25:54 +0200

    Bugfix

--- project/worker.py
+++ project/worker.py
@@ -4,7 +4,10 @@ import time
 from celery import Celery
 from celery.contrib import rdb
 
-from submission_runner import SubmissionRunner
+from file_operations import FileOperations
+from github_helper import GithubHelper
+from log_helper import LogHelper
+from submission_runner import SubmissionRunner, RepoResource
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
@@ -14,9 +17,15 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://lo
 @celery.task(name="fetch_solution_repo")
 def fetch_solution_repo(solution_repo_name):
     print("Fetching solution repo")
-    submission_runner = SubmissionRunner({'canvas_course_id': 0})
-    submission_runner.fetch_solution_repo(solution_repo_name)
-    return True
+
+    solution_repo_path = f'solution-repo'
+    log_helper = LogHelper()
+    file_ops = FileOperations(log_helper)
+    github_helper = GithubHelper(log_helper)
+
+    with RepoResource(solution_repo_name, solution_repo_path + '/' + solution_repo_name, file_ops,
+                      github_helper, False) as solution_repo:
+        return True
 
 
 @celery.task(name="run_tests_for_student")
